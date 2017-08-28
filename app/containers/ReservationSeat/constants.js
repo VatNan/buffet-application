07bad344f00444e4c5917a5540182ea64444255c
vattanan buranapugdee <vattanan.bu@gmail.com>
2017-08-28 14:58:28 +0700

    Add action creator in Reservation Seat

--- app/containers/ReservationSeat/constants.js
+++ app/containers/ReservationSeat/constants.js
@@ -5,3 +5,18 @@
  */
 
 export const DEFAULT_ACTION = 'app/ReservationSeat/DEFAULT_ACTION';
+
+export const GET_SEATS_REQUEST = 'app/ReservationSeat/GET_SEATS_REQUEST';
+export const GET_SEATS_SUCCESS = 'app/ReservationSeat/GET_SEATS_REQUEST';
+export const GET_SEATS_FAILURE = 'app/ReservationSeat/GET_SEATS_FAILURE';
+
+export const LISTENING_SEATS_SUCCESS = 'app/ReservationSeat/LISTENING_SEATS_SUCCESS';
+export const LISTENING_SEATS_FAILURE = 'app/ReservationSeat/LISTENING_SEATS_FAILURE';
+
+export const UPDATE_SEATS_REQUEST = 'app/ReservationSeat/UPDATE_SEATS_REQUEST';
+export const UPDATE_SEATS_SUCCESS = 'app/ReservationSeat/UPDATE_SEATS_REQUEST';
+export const UPDATE_SEATS_FAILURE = 'app/ReservationSeat/UPDATE_SEATS_FAILURE';
+
+
+
+

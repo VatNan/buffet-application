4685e86b39f8915c7625c220fa8f6fca8da67abf
vattanan buranapugdee <vattanan.bu@gmail.com>
2017-08-28 21:56:20 +0700

    Add constace in constances.js

---
+++ app/config/constances.js
@@ -0,0 +1,16 @@
+export const CLASS_BY_ZONE_SEAT = {
+    A: "col-4 col-sm-3 m-1",
+    B: "col-4 m-2"
+};
+
+export const TEXT_BY_STATUS_SEAT = {
+    free: 'ว่าง',
+    busy: 'ไม่ว่าง',
+    reserve: 'จอง'
+};
+
+export const BUTTON_COLOR_BY_STATUS_SEAT = {
+    free: 'secondary',
+    busy: 'danger',
+    reserve: 'warning'
+};

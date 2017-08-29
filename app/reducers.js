4fcc217a55153ad44940e95d447c771dbcdce5fc
vattanan buranapugdee <vattanan.bu@gmail.com>
2017-08-29 17:48:19 +0700

    Add global reducer

--- app/reducers.js
+++ app/reducers.js
@@ -9,6 +9,8 @@ import { LOCATION_CHANGE } from 'react-router-redux';
 
 import globalReducer from 'containers/App/reducer';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
+import manageSeatsReducer from './containers/ManageSeats/reducer';
+import createBillReducer from './containers/CreateBill/reducer';
 
 /*
  * routeReducer
@@ -46,6 +48,8 @@ export default function createReducer(asyncReducers) {
     route: routeReducer,
     global: globalReducer,
     language: languageProviderReducer,
+    manageSeats: manageSeatsReducer,
+    createBill: createBillReducer,
     ...asyncReducers,
   });
 }

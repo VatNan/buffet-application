c9243bec124e9207aca823c9963280e1f8c49bb0
vattanan buranapugdee <vattanan.bu@gmail.com>
2017-08-29 13:24:59 +0700

    Rename file

---
+++ app/containers/ManageSeats/ManageBlock.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
+import Card from '../../components/Card';
+import Block from '../../components/Block';
+import Label from '../../components/Label';
+
+function ManageBlock(props) {
+    return (
+        <Card
+            border="0px"
+            direction="column"
+            bgColor="azure"
+            padding="6px 0px"
+            alignItems="center"
+            haveShadow={true}
+        >
+            <Block
+                flex="1"
+                justifyContent="center"
+            >
+                <Button 
+                    color="primary"
+                    className="col-5 m-1" 
+                    size="lg"
+                >
+                    สร้างบิล
+                </Button>
+                {' '}
+
+                <Button 
+                    color="success"
+                    className="col-5 m-1" 
+                    size="lg"
+                >
+                    เช็คบิล
+                </Button>
+            </Block>
+
+            <Block
+                direction="column"
+                padding="10px"
+            >
+                <Label
+                    color="black"
+                    fontSize="20px"
+                >
+                    ที่นั่งทั้งหมด : {props.max}
+                </Label>
+                <Label
+                    color="black"
+                    fontSize="20px"
+                >
+                    ที่นั่งที่ว่างอยู่ : {props.total}
+                </Label> 
+            </Block>
+        </Card>
+    );
+}
+
+export default ManageBlock;

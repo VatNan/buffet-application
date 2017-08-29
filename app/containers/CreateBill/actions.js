9b61d553f68d9b6807519dcdd0087ef7f69cffc8
vattanan buranapugdee <vattanan.bu@gmail.com>
2017-08-29 20:55:02 +0700

    Add action and action creator in CreateBill

--- app/containers/CreateBill/actions.js
+++ app/containers/CreateBill/actions.js
@@ -6,10 +6,61 @@
 
 import {
   DEFAULT_ACTION,
+  CHECK_CUSTOMER_AMOUNT_REQUEST,
+  CHECK_CUSTOMER_AMOUNT_SUCCESS,
+  CHECK_CUSTOMER_AMOUNT_FAILURE,
+  CLEAR_ALL,
+  CHOOSE_SEATS_REQUEST,
+  CHOOSE_SEATS_SUCCESS,
+  CHOOSE_SEATS_FAILURE,
 } from './constants';
 
 export function defaultAction() {
   return {
     type: DEFAULT_ACTION,
   };
+}
+
+export function checkCustomerAmount(customerAmount) {
+  return {
+    type: CHECK_CUSTOMER_AMOUNT_REQUEST,
+    customerAmount
+  };
+}
+
+export function customerAmountPass() {
+  return {
+    type: CHECK_CUSTOMER_AMOUNT_SUCCESS,
+  };
+} 
+
+export function customerAmountFail() {
+  return {
+    type: CHECK_CUSTOMER_AMOUNT_FAILURE,
+  }
+}
+
+export function clearAll() {
+  return {
+    type: CLEAR_ALL
+  }
+}
+
+export function chooseSeats(seatsIsSelect) {
+  return {
+    type: CHOOSE_SEATS_REQUEST,
+    seatsIsSelect
+  }
+}
+
+export function chooseSeatsPass() {
+  return {
+    type: CHOOSE_SEATS_SUCCESS
+  }
+}
+
+export function chooseSeatsFail() {
+  return {
+    type: CHOOSE_SEATS_FAILURE
+  }
 }
